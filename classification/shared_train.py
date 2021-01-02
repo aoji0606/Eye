8a6e0038e37151493bbc469f59f2631ed0af250a
aoji-tjut <55875798+aoji-tjut@users.noreply.github.com>
2021-01-02 15:57:10 +0800

    Add files via upload

---
+++ classification/shared_train.py
@@ -0,0 +1,196 @@
+import os
+import sys
+import tensorflow as tf
+import tensorflow.keras.layers as layer
+from tensorflow.keras import layers
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import recall_score, precision_score, f1_score
+
+num = int(sys.argv[i])
+shape = 0
+ACTIVATION = "selu"
+shape_list = []
+k_s_list = []
+
+
+def ChannelAttention(conv):
+    avg = layer.GlobalAveragePooling2D()(conv)
+    max = layer.GlobalMaxPool2D()(conv)
+    avg = layer.Reshape((1, 1, avg.shape[1]))(avg)
+    max = layer.Reshape((1, 1, max.shape[1]))(max)
+    avg_out = layer.Conv2D(int(conv.shape[-1] / 16), kernel_size=1, strides=1, padding="same",
+                           kernel_regularizer=tf.keras.regularizers.l2(1e-4), use_bias=True, activation="relu")(avg)
+    avg_out = layer.Conv2D(int(conv.shape[-1]), kernel_size=1, strides=1, padding="same",
+                           kernel_regularizer=tf.keras.regularizers.l2(1e-4), use_bias=True)(avg_out)
+    max_out = layer.Conv2D(int(conv.shape[-1] / 16), kernel_size=1, strides=1, padding="same",
+                           kernel_regularizer=tf.keras.regularizers.l2(1e-4), use_bias=True, activation="relu")(max)
+    max_out = layer.Conv2D(conv.shape[-1], kernel_size=1, strides=1, padding="same",
+                           kernel_regularizer=tf.keras.regularizers.l2(1e-4), use_bias=True)(max_out)
+    out = avg_out + max_out
+    out = layer.Activation("sigmoid")(out)
+
+    return out
+
+
+def SpatialAttention(inputs):
+    avg_out = tf.reduce_mean(inputs, axis=3)
+    max_out = tf.reduce_max(inputs, axis=3)
+    out = tf.stack([avg_out, max_out], axis=3)
+    out = layers.Conv2D(filters=1, kernel_size=7, strides=1, activation="sigmoid", padding="same", use_bias=False,
+                        kernel_initializer="he_normal", kernel_regularizer=tf.keras.regularizers.l2(5e-4))(out)
+
+    return out
+
+
+def LoadData():
+    if not os.path.exists("model/shared_model.h5"):
+        global num
+        num = 19
+
+    X = np.load("./data/%d/X.npy" % num)
+    y = np.load("./data/%d/y.npy" % num)
+
+    global shape
+    shape = X.shape[1]
+    input = layer.Input([X.shape[1], X.shape[2], X.shape[3]])
+
+    while (len(shape_list) < 3):
+        for k in range(2, int(shape / 2) + 1, 1):
+            for s in range(2, k + 1, 1):
+                pool = layer.MaxPool2D(pool_size=(k, k), strides=(s, s))(input)
+                if pool.shape[1] % 2 and pool.shape[1] >= 5 and pool.shape[1] <= 9 and pool.shape[
+                    1] not in shape_list and len(shape_list) < 3:
+                    shape_list.append(pool.shape[1])
+                    k_s_list.append([k, s])
+        break
+
+    print(shape_list)
+    print(k_s_list)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=666)
+
+    return X_train, X_test, y_train, y_test
+
+
+def GetModel():
+    if os.path.exists("model/shared_model.h5"):
+        shared_model = tf.keras.models.load_model("model/shared_model.h5")
+        model = tf.keras.models.load_model("./model/model_%d.h5" % num)
+        for i in range(8, len(model.layers), 1):
+            model.layers[i] = shared_model.layers[i]
+    else:
+        arg = np.argsort(shape_list)
+        k_min = k_s_list[arg[0]][0]
+        s_min = k_s_list[arg[0]][1]
+        k_mid = k_s_list[arg[1]][0]
+        s_mid = k_s_list[arg[1]][1]
+        k_max = k_s_list[arg[2]][0]
+        s_max = k_s_list[arg[2]][1]
+
+        input = layer.Input(shape=[shape, shape, 3])
+
+        conv = layer.Conv2D(filters=32, kernel_size=3, strides=1, padding="same", activation=ACTIVATION,
+                            kernel_initializer=tf.keras.initializers.he_uniform())(input)
+
+        # SPP
+        pool_min = layer.MaxPool2D(pool_size=(k_min, k_min), strides=(s_min, s_min))(conv)
+        pool_mid = layer.MaxPool2D(pool_size=(k_mid, k_mid), strides=(s_mid, s_mid))(conv)
+        pool_max = layer.MaxPool2D(pool_size=(k_max, k_max), strides=(s_max, s_max))(conv)
+
+        # print(pool_max.shape)
+        # print(pool_mid.shape)
+        # print(pool_min.shape)
+
+        padding_mid = layer.ZeroPadding2D(1)(pool_mid)
+        padding_min = layer.ZeroPadding2D(2)(pool_min)
+
+        # print(pool_max.shape)
+        # print(padding_mid.shape)
+        # print(padding_min.shape)
+
+        add = layer.Add()([pool_max, padding_mid, padding_min])
+        # print(add.shape)
+
+        conv1 = layer.Conv2D(filters=64, kernel_size=3, strides=1, padding="valid", activation=ACTIVATION,
+                             kernel_initializer=tf.keras.initializers.he_uniform())(add)
+        conv2 = layer.Conv2D(filters=128, kernel_size=3, strides=1, padding="valid", activation=ACTIVATION,
+                             kernel_initializer=tf.keras.initializers.he_uniform())(conv1)
+        conv3 = layer.Conv2D(filters=256, kernel_size=1, strides=1, padding="valid", activation=ACTIVATION,
+                             kernel_initializer=tf.keras.initializers.he_uniform())(conv2)
+        conv1_c = layer.Conv2D(filters=256, kernel_size=1, strides=1, padding="valid", activation=ACTIVATION,
+                               kernel_initializer=tf.keras.initializers.he_uniform())(conv1)
+        conv2_c = layer.Conv2D(filters=256, kernel_size=1, strides=1, padding="valid", activation=ACTIVATION,
+                               kernel_initializer=tf.keras.initializers.he_uniform())(conv2)
+        conv3_c = layer.Conv2D(filters=256, kernel_size=1, strides=1, padding="valid", activation=ACTIVATION,
+                               kernel_initializer=tf.keras.initializers.he_uniform())(conv3)
+
+        # conv3_c Attention
+        out_channel = ChannelAttention(conv3_c)
+        out_channel = out_channel * conv3_c
+        out_spatial = SpatialAttention(out_channel)
+        out_cbam_conv3_c = out_spatial * out_channel
+
+        dconv1 = layer.Conv2DTranspose(filters=256, kernel_size=1, strides=1, padding="valid", activation=ACTIVATION)(
+            out_cbam_conv3_c)
+        add_dconv1_conv2_c = layer.Add()([dconv1, conv2_c])
+
+        # add_dconv1_conv2_c Attention
+        out_channel = ChannelAttention(add_dconv1_conv2_c)
+        out_channel = out_channel * add_dconv1_conv2_c
+        out_spatial = SpatialAttention(out_channel)
+        out_cbam_add_dconv1_conv2_c = out_spatial * out_channel
+
+        dconv2 = layer.Conv2DTranspose(filters=256, kernel_size=3, strides=1, padding="valid", activation=ACTIVATION)(
+            out_cbam_add_dconv1_conv2_c)
+        add_dconv2_conv1_c = layer.Add()([dconv2, conv1_c])
+        conv_temp = layer.Conv2D(filters=256, kernel_size=3, strides=1, padding="valid", activation=ACTIVATION,
+                                 kernel_initializer=tf.keras.initializers.he_uniform())(
+            add_dconv2_conv1_c)
+        add_final = layer.Add()([add_dconv1_conv2_c, conv_temp, conv3_c])
+        flatten = layer.Flatten()(add_final)
+        output = layer.Dense(1, activation="sigmoid", kernel_initializer=tf.keras.initializers.he_uniform())(flatten)
+
+        # Model
+        model = tf.keras.Model(inputs=input, outputs=output)
+        model.summary()
+
+    return model
+
+
+def Model(X_train, X_test, y_train, y_test):
+    model = GetModel()
+    model.compile(optimizer=tf.keras.optimizers.Adam(0.0001), loss="binary_crossentropy",
+                  metrics=[tf.keras.metrics.Precision(), tf.keras.metrics.Recall()])
+    history = model.fit(X_train, y_train, batch_size=64, epochs=50, validation_split=0.2)
+
+    model.evaluate(X_test, y_test)
+
+    model.save("./model/shared_model.h5")
+
+    # y_pre = model.predict(X_test)
+    # y_pre = y_pre.reshape(-1)
+    # y_pre[y_pre > 0.5] = 1
+    # y_pre[y_pre != 1] = 0
+    # y_test = y_test.reshape(-1)
+    #
+    # print(y_pre)
+    # print(y_test)
+    #
+    # print("precision_score: ", precision_score(y_test, y_pre))
+    # print("recall_score: ", recall_score(y_test, y_pre))
+    # print("f1_score: ", f1_score(y_test, y_pre))
+
+    pd.DataFrame(history.history).plot(figsize=(8, 5))
+    plt.grid(True)
+    plt.gca().set_ylim(0, 1)
+    # plt.show()
+    plt.savefig("./shared/result_%d.png" % num)
+
+
+if __name__ == '__main__':
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    X_train, X_test, y_train, y_test = LoadData()
+    Model(X_train, X_test, y_train, y_test)
--- classification/train.py
+++ classification/train.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import tensorflow as tf
 import tensorflow.keras.layers as layer
 from tensorflow.keras import layers
@@ -6,14 +7,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import recall_score, precision_score, f1_score
+from pprint import pprint
 
-num = 19
+num = int(sys.argv[1])
 shape = 0
 ACTIVATION = "selu"
 shape_list = []
 k_s_list = []
-
 
 def ChannelAttention(conv):
     avg = layer.GlobalAveragePooling2D()(conv)
@@ -45,10 +45,6 @@ def SpatialAttention(inputs):
 
 
 def LoadData():
-    if not os.path.exists("model/shared_model.h5"):
-        global num
-        num = 19
-
     X = np.load("./data/%d/X.npy" % num)
     y = np.load("./data/%d/y.npy" % num)
 
@@ -66,108 +62,99 @@ def LoadData():
                     k_s_list.append([k, s])
         break
 
-    print(shape_list)
-    print(k_s_list)
+    # print(shape_list)
+    # print(k_s_list)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=666)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
     return X_train, X_test, y_train, y_test
 
 
-def GetModel():
-    if os.path.exists("model/shared_model.h5"):
-        shared_model = tf.keras.models.load_model("model/shared_model.h5")
-        model = tf.keras.models.load_model("./model/model_%d.h5" % num)
-        for i in range(8, len(model.layers), 1):
-            model.layer[i] = shared_model.layer[i]
-    else:
-        arg = np.argsort(shape_list)
-        k_min = k_s_list[arg[0]][0]
-        s_min = k_s_list[arg[0]][1]
-        k_mid = k_s_list[arg[1]][0]
-        s_mid = k_s_list[arg[1]][1]
-        k_max = k_s_list[arg[2]][0]
-        s_max = k_s_list[arg[2]][1]
-
-        input = layer.Input(shape=[shape, shape, 3])
-
-        conv = layer.Conv2D(filters=32, kernel_size=3, strides=1, padding="same", activation=ACTIVATION,
-                            kernel_initializer=tf.keras.initializers.he_uniform())(input)
-
-        # SPP
-        pool_min = layer.MaxPool2D(pool_size=(k_min, k_min), strides=(s_min, s_min))(conv)
-        pool_mid = layer.MaxPool2D(pool_size=(k_mid, k_mid), strides=(s_mid, s_mid))(conv)
-        pool_max = layer.MaxPool2D(pool_size=(k_max, k_max), strides=(s_max, s_max))(conv)
-
-        # print(pool_max.shape)
-        # print(pool_mid.shape)
-        # print(pool_min.shape)
-
-        padding_mid = layer.ZeroPadding2D(1)(pool_mid)
-        padding_min = layer.ZeroPadding2D(2)(pool_min)
-
-        # print(pool_max.shape)
-        # print(padding_mid.shape)
-        # print(padding_min.shape)
-
-        add = layer.Add()([pool_max, padding_mid, padding_min])
-        # print(add.shape)
-
-        conv1 = layer.Conv2D(filters=64, kernel_size=3, strides=1, padding="valid", activation=ACTIVATION,
-                             kernel_initializer=tf.keras.initializers.he_uniform())(add)
-        conv2 = layer.Conv2D(filters=128, kernel_size=3, strides=1, padding="valid", activation=ACTIVATION,
-                             kernel_initializer=tf.keras.initializers.he_uniform())(conv1)
-        conv3 = layer.Conv2D(filters=256, kernel_size=1, strides=1, padding="valid", activation=ACTIVATION,
-                             kernel_initializer=tf.keras.initializers.he_uniform())(conv2)
-        conv1_c = layer.Conv2D(filters=256, kernel_size=1, strides=1, padding="valid", activation=ACTIVATION,
-                               kernel_initializer=tf.keras.initializers.he_uniform())(conv1)
-        conv2_c = layer.Conv2D(filters=256, kernel_size=1, strides=1, padding="valid", activation=ACTIVATION,
-                               kernel_initializer=tf.keras.initializers.he_uniform())(conv2)
-        conv3_c = layer.Conv2D(filters=256, kernel_size=1, strides=1, padding="valid", activation=ACTIVATION,
-                               kernel_initializer=tf.keras.initializers.he_uniform())(conv3)
-
-        # conv3_c Attention
-        out_channel = ChannelAttention(conv3_c)
-        out_channel = out_channel * conv3_c
-        out_spatial = SpatialAttention(out_channel)
-        out_cbam_conv3_c = out_spatial * out_channel
-
-        dconv1 = layer.Conv2DTranspose(filters=256, kernel_size=1, strides=1, padding="valid", activation=ACTIVATION)(
-            out_cbam_conv3_c)
-        add_dconv1_conv2_c = layer.Add()([dconv1, conv2_c])
-
-        # add_dconv1_conv2_c Attention
-        out_channel = ChannelAttention(add_dconv1_conv2_c)
-        out_channel = out_channel * add_dconv1_conv2_c
-        out_spatial = SpatialAttention(out_channel)
-        out_cbam_add_dconv1_conv2_c = out_spatial * out_channel
-
-        dconv2 = layer.Conv2DTranspose(filters=256, kernel_size=3, strides=1, padding="valid", activation=ACTIVATION)(
-            out_cbam_add_dconv1_conv2_c)
-        add_dconv2_conv1_c = layer.Add()([dconv2, conv1_c])
-        conv_temp = layer.Conv2D(filters=256, kernel_size=3, strides=1, padding="valid", activation=ACTIVATION,
-                                 kernel_initializer=tf.keras.initializers.he_uniform())(
-            add_dconv2_conv1_c)
-        add_final = layer.Add()([add_dconv1_conv2_c, conv_temp, conv3_c])
-        flatten = layer.Flatten()(add_final)
-        output = layer.Dense(1, activation="sigmoid", kernel_initializer=tf.keras.initializers.he_uniform())(flatten)
-
-        # Model
-        model = tf.keras.Model(inputs=input, outputs=output)
-
-    return model
-
-
 def Model(X_train, X_test, y_train, y_test):
-    model = GetModel()
+    arg = np.argsort(shape_list)
+    k_min = k_s_list[arg[0]][0]
+    s_min = k_s_list[arg[0]][1]
+    k_mid = k_s_list[arg[1]][0]
+    s_mid = k_s_list[arg[1]][1]
+    k_max = k_s_list[arg[2]][0]
+    s_max = k_s_list[arg[2]][1]
+
+    input = layer.Input(shape=[shape, shape, 3])
+
+    conv = layer.Conv2D(filters=32, kernel_size=3, strides=1, padding="same", activation=ACTIVATION,
+                        kernel_initializer=tf.keras.initializers.he_uniform())(input)
+
+    # SPP
+    pool_min = layer.MaxPool2D(pool_size=(k_min, k_min), strides=(s_min, s_min))(conv)
+    pool_mid = layer.MaxPool2D(pool_size=(k_mid, k_mid), strides=(s_mid, s_mid))(conv)
+    pool_max = layer.MaxPool2D(pool_size=(k_max, k_max), strides=(s_max, s_max))(conv)
+
+    # print(pool_max.shape)
+    # print(pool_mid.shape)
+    # print(pool_min.shape)
+
+    pool_mid = layer.ZeroPadding2D(1)(pool_mid)
+    pool_min = layer.ZeroPadding2D(2)(pool_min)
+
+    # print(pool_max.shape)
+    # print(pool_mid.shape)
+    # print(pool_min.shape)
+
+    input_fcn = layer.Add()([pool_max, pool_mid, pool_min])
+    print(input_fcn.shape)
+
+    # FCN
+    conv1 = layer.Conv2D(filters=64, kernel_size=3, strides=1, padding="valid", activation=ACTIVATION,
+                         kernel_initializer=tf.keras.initializers.he_uniform())(input_fcn)
+    conv2 = layer.Conv2D(filters=128, kernel_size=3, strides=1, padding="valid", activation=ACTIVATION,
+                         kernel_initializer=tf.keras.initializers.he_uniform())(conv1)
+    conv3 = layer.Conv2D(filters=256, kernel_size=1, strides=1, padding="valid", activation=ACTIVATION,
+                         kernel_initializer=tf.keras.initializers.he_uniform())(conv2)
+    conv1_c = layer.Conv2D(filters=256, kernel_size=1, strides=1, padding="valid", activation=ACTIVATION,
+                           kernel_initializer=tf.keras.initializers.he_uniform())(conv1)
+    conv2_c = layer.Conv2D(filters=256, kernel_size=1, strides=1, padding="valid", activation=ACTIVATION,
+                           kernel_initializer=tf.keras.initializers.he_uniform())(conv2)
+    conv3_c = layer.Conv2D(filters=256, kernel_size=1, strides=1, padding="valid", activation=ACTIVATION,
+                           kernel_initializer=tf.keras.initializers.he_uniform())(conv3)
+
+    # conv3_c Attention
+    out_channel = ChannelAttention(conv3_c)
+    out_channel = out_channel * conv3_c
+    out_spatial = SpatialAttention(out_channel)
+    out_cbam_conv3_c = out_spatial * out_channel
+
+    dconv1 = layer.Conv2DTranspose(filters=256, kernel_size=1, strides=1, padding="valid", activation=ACTIVATION)(
+        out_cbam_conv3_c)
+    add_dconv1_conv2_c = layer.Add()([dconv1, conv2_c])
+
+    # add_dconv1_conv2_c Attention
+    out_channel = ChannelAttention(add_dconv1_conv2_c)
+    out_channel = out_channel * add_dconv1_conv2_c
+    out_spatial = SpatialAttention(out_channel)
+    out_cbam_add_dconv1_conv2_c = out_spatial * out_channel
+    # [5,5,256] = [5,5,1] * [5,5,256]
+
+    dconv2 = layer.Conv2DTranspose(filters=256, kernel_size=3, strides=1, padding="valid", activation=ACTIVATION)(
+        out_cbam_add_dconv1_conv2_c)
+    add_dconv2_conv1_c = layer.Add()([dconv2, conv1_c])
+    conv_temp = layer.Conv2D(filters=256, kernel_size=3, strides=1, padding="valid", activation=ACTIVATION,
+                             kernel_initializer=tf.keras.initializers.he_uniform())(
+        add_dconv2_conv1_c)
+    add = layer.Add()([add_dconv1_conv2_c, conv_temp, conv3_c])
+    flatten = layer.Flatten()(add)
+    output = layer.Dense(1, activation="sigmoid", kernel_initializer=tf.keras.initializers.he_uniform())(flatten)
+
+    # Model
+    model = tf.keras.Model(inputs=input, outputs=output)
+
     model.summary()
     model.compile(optimizer=tf.keras.optimizers.Adam(0.0001), loss="binary_crossentropy",
                   metrics=[tf.keras.metrics.Precision(), tf.keras.metrics.Recall()])
-    history = model.fit(X_train, y_train, batch_size=64, epochs=1, validation_split=0.2)
+    history = model.fit(X_train, y_train, batch_size=64, epochs=100, validation_split=0.2)
+
+    model.save("./model/model_%d.h5" % num)
 
     model.evaluate(X_test, y_test)
-
-    model.save("./model/shared_model.h5")
 
     # y_pre = model.predict(X_test)
     # y_pre = y_pre.reshape(-1)
@@ -186,7 +173,7 @@ def Model(X_train, X_test, y_train, y_test):
     plt.grid(True)
     plt.gca().set_ylim(0, 1)
     # plt.show()
-    plt.savefig("./result_%d.png" % num)
+    plt.savefig("./unique/result_%d.png" % num)
 
 
 if __name__ == '__main__':
