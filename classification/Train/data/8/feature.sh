42ceafb83de9124f64d923bdd2b304ca7b9b0f93
aoji-tjut <245611640@qq.com>
2021-01-20 21:26:56 +0800

    1.20

--- classification/Train/data/8/feature.sh
+++
@@ -1,8 +0,0 @@
-python feature.py 19
-python feature.py 21
-python feature.py 23
-python feature.py 25
-python feature.py 27
-python feature.py 29
-python feature.py 31
-python feature.py 33
--- classification/Train/data/8/shared_train.sh
+++
@@ -1,8 +0,0 @@
-python shared_train.py 19
-python shared_train.py 21
-python shared_train.py 23
-python shared_train.py 25
-python shared_train.py 27
-python shared_train.py 29
-python shared_train.py 31
-python shared_train.py 33
