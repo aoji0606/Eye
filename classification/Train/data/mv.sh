a454d4dac9a5151435d54dc8f3272f493f5f0dd3
aoji-tjut <245611640@qq.com>
2021-01-20 21:21:54 +0800

    1.20

---
+++ classification/Train/data/mv.sh
@@ -0,0 +1,404 @@
+mv 1/1/*19.mat 1/1/19/
+mv 1/1/*21.mat 1/1/21/
+mv 1/1/*23.mat 1/1/23/
+mv 1/1/*25.mat 1/1/25/
+mv 1/1/*27.mat 1/1/27/
+mv 1/1/*29.mat 1/1/29/
+mv 1/1/*31.mat 1/1/31/
+mv 1/1/*33.mat 1/1/33/
+
+mv 1/2/*19.mat 1/2/19/
+mv 1/2/*21.mat 1/2/21/
+mv 1/2/*23.mat 1/2/23/
+mv 1/2/*25.mat 1/2/25/
+mv 1/2/*27.mat 1/2/27/
+mv 1/2/*29.mat 1/2/29/
+mv 1/2/*31.mat 1/2/31/
+mv 1/2/*33.mat 1/2/33/
+
+mv 1/3/*19.mat 1/3/19/
+mv 1/3/*21.mat 1/3/21/
+mv 1/3/*23.mat 1/3/23/
+mv 1/3/*25.mat 1/3/25/
+mv 1/3/*27.mat 1/3/27/
+mv 1/3/*29.mat 1/3/29/
+mv 1/3/*31.mat 1/3/31/
+mv 1/3/*33.mat 1/3/33/
+
+mv 1/4/*19.mat 1/4/19/
+mv 1/4/*21.mat 1/4/21/
+mv 1/4/*23.mat 1/4/23/
+mv 1/4/*25.mat 1/4/25/
+mv 1/4/*27.mat 1/4/27/
+mv 1/4/*29.mat 1/4/29/
+mv 1/4/*31.mat 1/4/31/
+mv 1/4/*33.mat 1/4/33/
+
+mv 1/5/*19.mat 1/5/19/
+mv 1/5/*21.mat 1/5/21/
+mv 1/5/*23.mat 1/5/23/
+mv 1/5/*25.mat 1/5/25/
+mv 1/5/*27.mat 1/5/27/
+mv 1/5/*29.mat 1/5/29/
+mv 1/5/*31.mat 1/5/31/
+mv 1/5/*33.mat 1/5/33/
+
+mv 2/1/*19.mat 2/1/19/
+mv 2/1/*21.mat 2/1/21/
+mv 2/1/*23.mat 2/1/23/
+mv 2/1/*25.mat 2/1/25/
+mv 2/1/*27.mat 2/1/27/
+mv 2/1/*29.mat 2/1/29/
+mv 2/1/*31.mat 2/1/31/
+mv 2/1/*33.mat 2/1/33/
+
+mv 2/2/*19.mat 2/2/19/
+mv 2/2/*21.mat 2/2/21/
+mv 2/2/*23.mat 2/2/23/
+mv 2/2/*25.mat 2/2/25/
+mv 2/2/*27.mat 2/2/27/
+mv 2/2/*29.mat 2/2/29/
+mv 2/2/*31.mat 2/2/31/
+mv 2/2/*33.mat 2/2/33/
+
+mv 2/3/*19.mat 2/3/19/
+mv 2/3/*21.mat 2/3/21/
+mv 2/3/*23.mat 2/3/23/
+mv 2/3/*25.mat 2/3/25/
+mv 2/3/*27.mat 2/3/27/
+mv 2/3/*29.mat 2/3/29/
+mv 2/3/*31.mat 2/3/31/
+mv 2/3/*33.mat 2/3/33/
+
+mv 2/4/*19.mat 2/4/19/
+mv 2/4/*21.mat 2/4/21/
+mv 2/4/*23.mat 2/4/23/
+mv 2/4/*25.mat 2/4/25/
+mv 2/4/*27.mat 2/4/27/
+mv 2/4/*29.mat 2/4/29/
+mv 2/4/*31.mat 2/4/31/
+mv 2/4/*33.mat 2/4/33/
+
+mv 2/5/*19.mat 2/5/19/
+mv 2/5/*21.mat 2/5/21/
+mv 2/5/*23.mat 2/5/23/
+mv 2/5/*25.mat 2/5/25/
+mv 2/5/*27.mat 2/5/27/
+mv 2/5/*29.mat 2/5/29/
+mv 2/5/*31.mat 2/5/31/
+mv 2/5/*33.mat 2/5/33/
+
+mv 3/1/*19.mat 3/1/19/
+mv 3/1/*21.mat 3/1/21/
+mv 3/1/*23.mat 3/1/23/
+mv 3/1/*25.mat 3/1/25/
+mv 3/1/*27.mat 3/1/27/
+mv 3/1/*29.mat 3/1/29/
+mv 3/1/*31.mat 3/1/31/
+mv 3/1/*33.mat 3/1/33/
+
+mv 3/2/*19.mat 3/2/19/
+mv 3/2/*21.mat 3/2/21/
+mv 3/2/*23.mat 3/2/23/
+mv 3/2/*25.mat 3/2/25/
+mv 3/2/*27.mat 3/2/27/
+mv 3/2/*29.mat 3/2/29/
+mv 3/2/*31.mat 3/2/31/
+mv 3/2/*33.mat 3/2/33/
+
+mv 3/3/*19.mat 3/3/19/
+mv 3/3/*21.mat 3/3/21/
+mv 3/3/*23.mat 3/3/23/
+mv 3/3/*25.mat 3/3/25/
+mv 3/3/*27.mat 3/3/27/
+mv 3/3/*29.mat 3/3/29/
+mv 3/3/*31.mat 3/3/31/
+mv 3/3/*33.mat 3/3/33/
+
+mv 3/4/*19.mat 3/4/19/
+mv 3/4/*21.mat 3/4/21/
+mv 3/4/*23.mat 3/4/23/
+mv 3/4/*25.mat 3/4/25/
+mv 3/4/*27.mat 3/4/27/
+mv 3/4/*29.mat 3/4/29/
+mv 3/4/*31.mat 3/4/31/
+mv 3/4/*33.mat 3/4/33/
+
+mv 3/5/*19.mat 3/5/19/
+mv 3/5/*21.mat 3/5/21/
+mv 3/5/*23.mat 3/5/23/
+mv 3/5/*25.mat 3/5/25/
+mv 3/5/*27.mat 3/5/27/
+mv 3/5/*29.mat 3/5/29/
+mv 3/5/*31.mat 3/5/31/
+mv 3/5/*33.mat 3/5/33/
+
+mv 4/1/*19.mat 4/1/19/
+mv 4/1/*21.mat 4/1/21/
+mv 4/1/*23.mat 4/1/23/
+mv 4/1/*25.mat 4/1/25/
+mv 4/1/*27.mat 4/1/27/
+mv 4/1/*29.mat 4/1/29/
+mv 4/1/*31.mat 4/1/31/
+mv 4/1/*33.mat 4/1/33/
+
+mv 4/2/*19.mat 4/2/19/
+mv 4/2/*21.mat 4/2/21/
+mv 4/2/*23.mat 4/2/23/
+mv 4/2/*25.mat 4/2/25/
+mv 4/2/*27.mat 4/2/27/
+mv 4/2/*29.mat 4/2/29/
+mv 4/2/*31.mat 4/2/31/
+mv 4/2/*33.mat 4/2/33/
+
+mv 4/3/*19.mat 4/3/19/
+mv 4/3/*21.mat 4/3/21/
+mv 4/3/*23.mat 4/3/23/
+mv 4/3/*25.mat 4/3/25/
+mv 4/3/*27.mat 4/3/27/
+mv 4/3/*29.mat 4/3/29/
+mv 4/3/*31.mat 4/3/31/
+mv 4/3/*33.mat 4/3/33/
+
+mv 4/4/*19.mat 4/4/19/
+mv 4/4/*21.mat 4/4/21/
+mv 4/4/*23.mat 4/4/23/
+mv 4/4/*25.mat 4/4/25/
+mv 4/4/*27.mat 4/4/27/
+mv 4/4/*29.mat 4/4/29/
+mv 4/4/*31.mat 4/4/31/
+mv 4/4/*33.mat 4/4/33/
+
+mv 4/5/*19.mat 4/5/19/
+mv 4/5/*21.mat 4/5/21/
+mv 4/5/*23.mat 4/5/23/
+mv 4/5/*25.mat 4/5/25/
+mv 4/5/*27.mat 4/5/27/
+mv 4/5/*29.mat 4/5/29/
+mv 4/5/*31.mat 4/5/31/
+mv 4/5/*33.mat 4/5/33/
+
+mv 5/1/*19.mat 5/1/19/
+mv 5/1/*21.mat 5/1/21/
+mv 5/1/*23.mat 5/1/23/
+mv 5/1/*25.mat 5/1/25/
+mv 5/1/*27.mat 5/1/27/
+mv 5/1/*29.mat 5/1/29/
+mv 5/1/*31.mat 5/1/31/
+mv 5/1/*33.mat 5/1/33/
+
+mv 5/2/*19.mat 5/2/19/
+mv 5/2/*21.mat 5/2/21/
+mv 5/2/*23.mat 5/2/23/
+mv 5/2/*25.mat 5/2/25/
+mv 5/2/*27.mat 5/2/27/
+mv 5/2/*29.mat 5/2/29/
+mv 5/2/*31.mat 5/2/31/
+mv 5/2/*33.mat 5/2/33/
+
+mv 5/3/*19.mat 5/3/19/
+mv 5/3/*21.mat 5/3/21/
+mv 5/3/*23.mat 5/3/23/
+mv 5/3/*25.mat 5/3/25/
+mv 5/3/*27.mat 5/3/27/
+mv 5/3/*29.mat 5/3/29/
+mv 5/3/*31.mat 5/3/31/
+mv 5/3/*33.mat 5/3/33/
+
+mv 5/4/*19.mat 5/4/19/
+mv 5/4/*21.mat 5/4/21/
+mv 5/4/*23.mat 5/4/23/
+mv 5/4/*25.mat 5/4/25/
+mv 5/4/*27.mat 5/4/27/
+mv 5/4/*29.mat 5/4/29/
+mv 5/4/*31.mat 5/4/31/
+mv 5/4/*33.mat 5/4/33/
+
+mv 5/5/*19.mat 5/5/19/
+mv 5/5/*21.mat 5/5/21/
+mv 5/5/*23.mat 5/5/23/
+mv 5/5/*25.mat 5/5/25/
+mv 5/5/*27.mat 5/5/27/
+mv 5/5/*29.mat 5/5/29/
+mv 5/5/*31.mat 5/5/31/
+mv 5/5/*33.mat 5/5/33/
+
+mv 6/1/*19.mat 6/1/19/
+mv 6/1/*21.mat 6/1/21/
+mv 6/1/*23.mat 6/1/23/
+mv 6/1/*25.mat 6/1/25/
+mv 6/1/*27.mat 6/1/27/
+mv 6/1/*29.mat 6/1/29/
+mv 6/1/*31.mat 6/1/31/
+mv 6/1/*33.mat 6/1/33/
+
+mv 6/2/*19.mat 6/2/19/
+mv 6/2/*21.mat 6/2/21/
+mv 6/2/*23.mat 6/2/23/
+mv 6/2/*25.mat 6/2/25/
+mv 6/2/*27.mat 6/2/27/
+mv 6/2/*29.mat 6/2/29/
+mv 6/2/*31.mat 6/2/31/
+mv 6/2/*33.mat 6/2/33/
+
+mv 6/3/*19.mat 6/3/19/
+mv 6/3/*21.mat 6/3/21/
+mv 6/3/*23.mat 6/3/23/
+mv 6/3/*25.mat 6/3/25/
+mv 6/3/*27.mat 6/3/27/
+mv 6/3/*29.mat 6/3/29/
+mv 6/3/*31.mat 6/3/31/
+mv 6/3/*33.mat 6/3/33/
+
+mv 6/4/*19.mat 6/4/19/
+mv 6/4/*21.mat 6/4/21/
+mv 6/4/*23.mat 6/4/23/
+mv 6/4/*25.mat 6/4/25/
+mv 6/4/*27.mat 6/4/27/
+mv 6/4/*29.mat 6/4/29/
+mv 6/4/*31.mat 6/4/31/
+mv 6/4/*33.mat 6/4/33/
+
+mv 6/5/*19.mat 6/5/19/
+mv 6/5/*21.mat 6/5/21/
+mv 6/5/*23.mat 6/5/23/
+mv 6/5/*25.mat 6/5/25/
+mv 6/5/*27.mat 6/5/27/
+mv 6/5/*29.mat 6/5/29/
+mv 6/5/*31.mat 6/5/31/
+mv 6/5/*33.mat 6/5/33/
+
+mv 7/1/*19.mat 7/1/19/
+mv 7/1/*21.mat 7/1/21/
+mv 7/1/*23.mat 7/1/23/
+mv 7/1/*25.mat 7/1/25/
+mv 7/1/*27.mat 7/1/27/
+mv 7/1/*29.mat 7/1/29/
+mv 7/1/*31.mat 7/1/31/
+mv 7/1/*33.mat 7/1/33/
+
+mv 7/2/*19.mat 7/2/19/
+mv 7/2/*21.mat 7/2/21/
+mv 7/2/*23.mat 7/2/23/
+mv 7/2/*25.mat 7/2/25/
+mv 7/2/*27.mat 7/2/27/
+mv 7/2/*29.mat 7/2/29/
+mv 7/2/*31.mat 7/2/31/
+mv 7/2/*33.mat 7/2/33/
+
+mv 7/3/*19.mat 7/3/19/
+mv 7/3/*21.mat 7/3/21/
+mv 7/3/*23.mat 7/3/23/
+mv 7/3/*25.mat 7/3/25/
+mv 7/3/*27.mat 7/3/27/
+mv 7/3/*29.mat 7/3/29/
+mv 7/3/*31.mat 7/3/31/
+mv 7/3/*33.mat 7/3/33/
+
+mv 7/4/*19.mat 7/4/19/
+mv 7/4/*21.mat 7/4/21/
+mv 7/4/*23.mat 7/4/23/
+mv 7/4/*25.mat 7/4/25/
+mv 7/4/*27.mat 7/4/27/
+mv 7/4/*29.mat 7/4/29/
+mv 7/4/*31.mat 7/4/31/
+mv 7/4/*33.mat 7/4/33/
+
+mv 7/5/*19.mat 7/5/19/
+mv 7/5/*21.mat 7/5/21/
+mv 7/5/*23.mat 7/5/23/
+mv 7/5/*25.mat 7/5/25/
+mv 7/5/*27.mat 7/5/27/
+mv 7/5/*29.mat 7/5/29/
+mv 7/5/*31.mat 7/5/31/
+mv 7/5/*33.mat 7/5/33/
+
+mv 8/1/training/*19.mat 8/1/training/19/
+mv 8/1/training/*21.mat 8/1/training/21/
+mv 8/1/training/*23.mat 8/1/training/23/
+mv 8/1/training/*25.mat 8/1/training/25/
+mv 8/1/training/*27.mat 8/1/training/27/
+mv 8/1/training/*29.mat 8/1/training/29/
+mv 8/1/training/*31.mat 8/1/training/31/
+mv 8/1/training/*33.mat 8/1/training/33/
+
+mv 8/2/training/*19.mat 8/2/training/19/
+mv 8/2/training/*21.mat 8/2/training/21/
+mv 8/2/training/*23.mat 8/2/training/23/
+mv 8/2/training/*25.mat 8/2/training/25/
+mv 8/2/training/*27.mat 8/2/training/27/
+mv 8/2/training/*29.mat 8/2/training/29/
+mv 8/2/training/*31.mat 8/2/training/31/
+mv 8/2/training/*33.mat 8/2/training/33/
+
+mv 8/3/training/*19.mat 8/3/training/19/
+mv 8/3/training/*21.mat 8/3/training/21/
+mv 8/3/training/*23.mat 8/3/training/23/
+mv 8/3/training/*25.mat 8/3/training/25/
+mv 8/3/training/*27.mat 8/3/training/27/
+mv 8/3/training/*29.mat 8/3/training/29/
+mv 8/3/training/*31.mat 8/3/training/31/
+mv 8/3/training/*33.mat 8/3/training/33/
+
+mv 8/4/training/*19.mat 8/4/training/19/
+mv 8/4/training/*21.mat 8/4/training/21/
+mv 8/4/training/*23.mat 8/4/training/23/
+mv 8/4/training/*25.mat 8/4/training/25/
+mv 8/4/training/*27.mat 8/4/training/27/
+mv 8/4/training/*29.mat 8/4/training/29/
+mv 8/4/training/*31.mat 8/4/training/31/
+mv 8/4/training/*33.mat 8/4/training/33/
+
+mv 8/5/training/*19.mat 8/5/training/19/
+mv 8/5/training/*21.mat 8/5/training/21/
+mv 8/5/training/*23.mat 8/5/training/23/
+mv 8/5/training/*25.mat 8/5/training/25/
+mv 8/5/training/*27.mat 8/5/training/27/
+mv 8/5/training/*29.mat 8/5/training/29/
+mv 8/5/training/*31.mat 8/5/training/31/
+mv 8/5/training/*33.mat 8/5/training/33/
+
+mv 8/1/test/*19.mat 8/1/test/19/
+mv 8/1/test/*21.mat 8/1/test/21/
+mv 8/1/test/*23.mat 8/1/test/23/
+mv 8/1/test/*25.mat 8/1/test/25/
+mv 8/1/test/*27.mat 8/1/test/27/
+mv 8/1/test/*29.mat 8/1/test/29/
+mv 8/1/test/*31.mat 8/1/test/31/
+mv 8/1/test/*33.mat 8/1/test/33/
+
+mv 8/2/test/*19.mat 8/2/test/19/
+mv 8/2/test/*21.mat 8/2/test/21/
+mv 8/2/test/*23.mat 8/2/test/23/
+mv 8/2/test/*25.mat 8/2/test/25/
+mv 8/2/test/*27.mat 8/2/test/27/
+mv 8/2/test/*29.mat 8/2/test/29/
+mv 8/2/test/*31.mat 8/2/test/31/
+mv 8/2/test/*33.mat 8/2/test/33/
+
+mv 8/3/test/*19.mat 8/3/test/19/
+mv 8/3/test/*21.mat 8/3/test/21/
+mv 8/3/test/*23.mat 8/3/test/23/
+mv 8/3/test/*25.mat 8/3/test/25/
+mv 8/3/test/*27.mat 8/3/test/27/
+mv 8/3/test/*29.mat 8/3/test/29/
+mv 8/3/test/*31.mat 8/3/test/31/
+mv 8/3/test/*33.mat 8/3/test/33/
+
+mv 8/4/test/*19.mat 8/4/test/19/
+mv 8/4/test/*21.mat 8/4/test/21/
+mv 8/4/test/*23.mat 8/4/test/23/
+mv 8/4/test/*25.mat 8/4/test/25/
+mv 8/4/test/*27.mat 8/4/test/27/
+mv 8/4/test/*29.mat 8/4/test/29/
+mv 8/4/test/*31.mat 8/4/test/31/
+mv 8/4/test/*33.mat 8/4/test/33/
+
+mv 8/5/test/*19.mat 8/5/test/19/
+mv 8/5/test/*21.mat 8/5/test/21/
+mv 8/5/test/*23.mat 8/5/test/23/
+mv 8/5/test/*25.mat 8/5/test/25/
+mv 8/5/test/*27.mat 8/5/test/27/
+mv 8/5/test/*29.mat 8/5/test/29/
+mv 8/5/test/*31.mat 8/5/test/31/
+mv 8/5/test/*33.mat 8/5/test/33/
